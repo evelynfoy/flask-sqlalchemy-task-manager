37717cd2a82e6b8a34368bf1a6b8c0c836c51f21
evelynfoy <evelyn.foy@hotmail.com>
2022-04-04 12:38:56 +0000

    Add home page with styling

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -3,7 +3,6 @@ from taskmanager import app, db
 from taskmanager.models import Category, Task
 
 
-
 @app.route("/")
 def home():
-    return render_template("base.html")
+    return render_template("tasks.html")
