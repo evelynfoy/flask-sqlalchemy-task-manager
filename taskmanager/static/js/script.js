a585e800ecfa23073a885299d06626877e07755c
evelynfoy <evelyn.foy@hotmail.com>
2022-04-10 18:55:14 +0000

    Add View Tasks functionality

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,16 +1,22 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // sidenav initialisation
-    let sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav);
+document.addEventListener('DOMContentLoaded', function () {
+  // sidenav initialisation
+  let sidenav = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(sidenav);
 
-    // datepicker initialisation
-    let datepicker = document.querySelectorAll('.datepicker');
-    M.Datepicker.init(datepicker, {
-      format: "dd mmmm, yyyy",
-      i18n: {done: "Select"}
-    });
-
-    // select 
-    let selects = document.querySelectorAll('select');
-    M.FormSelect.init(selects);
+  // datepicker initialisation
+  let datepicker = document.querySelectorAll('.datepicker');
+  M.Datepicker.init(datepicker, {
+    format: "dd mmmm, yyyy",
+    i18n: {
+      done: "Select"
+    }
   });
+
+  // select 
+  let selects = document.querySelectorAll('select');
+  M.FormSelect.init(selects);
+
+  // collapsible initialisation
+  let collapsibles = document.querySelectorAll('.collapsible');
+  M.Collapsible.init(collapsibles);
+});
