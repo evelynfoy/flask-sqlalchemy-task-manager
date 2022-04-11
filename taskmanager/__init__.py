7afac41a01656839b9d630bfb88ff925f35e4e58
evelynfoy <evelyn.foy@hotmail.com>
2022-04-11 08:00:51 +0000

    Correct spelling of DATABASE_URL

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -5,12 +5,14 @@ from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
     import env  # noqa
 
+
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+
 if os.environ.get("DEVELOPMENT") == "True":
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")  # local
 else:
-    uri = os.environ.get("DATABASE_URI")
+    uri = os.environ.get("DATABASE_URL")
     if uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
     app.config["SQLALCHEMY_DATABASE_URI"] = uri  # heroku
