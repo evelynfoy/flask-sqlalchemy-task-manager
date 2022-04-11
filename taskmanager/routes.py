afac214f79bf3ff31125e25c17aba6c4ff1df225
evelynfoy <evelyn.foy@hotmail.com>
2022-04-11 06:29:04 +0000

    Add delete task functionality

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -73,3 +73,12 @@ def edit_task(task_id):
         task.category_id = request.form.get("category_id")
         db.session.commit()
     return render_template("edit_task.html", task=task, categories=categories)
+
+
+@app.route("/delete_task/<int:task_id>", methods=["GET", "POST"])
+def delete_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    print(task)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for("home"))
